6dbaeff77097744c2e1a4ad7d1ea596fe38ff863
MaquinaDeFuego <jersonmorales02@gmail.com>
2021-10-21 15:59:10 -0600

    Primer commit en mi vida

---
+++ Clases/Clsproducto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+   public class Clsproducto
+    {
+       public List<tbl_producto> productos()
+       {
+           try
+           {
+               BaseDeDatosDataContext dc = new BaseDeDatosDataContext();
+               List<tbl_producto> ListaProducto = (from _productos in dc.tbl_producto select _productos).ToList();
+               return ListaProducto;
+           }
+           catch (Exception)
+           {
+               
+               return new List<tbl_producto>();
+           }
+       }
+
+    }
+}
---
+++ Contabanca1.0/tb_formventacredito.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Contabanca1._0
+{
+    class tb_formventacredito
+    {
+    }
+}
